38bb42592af4c3d223deda19ced9a3af734dc628
Iurii Ovsiannikov <jgoldste@z1t3c2.42bangkok.com>
2025-01-20 18:06:46 +0700

    ex00 - ex03 are done

---
+++ python00/ex03/NULL_not_found.py
@@ -0,0 +1,17 @@
+def NULL_not_found(object: any) -> int:
+    if object is None:
+        print(f'Nothing: {object} {type(object)}')
+        return 0
+    elif isinstance(object, float) and object != object:
+        print(f'NaN: {object} {type(object)}')
+        return 0
+    elif isinstance(object, int) and object == 0:
+        print(f'Zero: {object} {type(object)}')
+        return 0
+    elif isinstance(object, str) and object == '':
+        print(f'Empty: {object} {type(object)}')
+        return 0
+    elif isinstance(object, bool) and object == False:
+        print(f'Fake: {object} {type(object)}')
+        return 0
+    return 1
