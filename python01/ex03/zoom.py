0179a09097f1776758c8582a85cfc013b63de971
Iurii Ovsiannikov <jgoldste@z1t6c2.42bangkok.com>
2025-01-24 18:57:04 +0700

    ex03 is done

--- python01/ex03/zoom.py
+++ python01/ex03/zoom.py
@@ -1,8 +1,22 @@
 import numpy as np
-import PIL.Image
 
 
-def zoom(np_image: np.ndarray) -> np.ndarray:
+def zoom_1024x768_to_400x400(np_image: np.ndarray) -> np.ndarray:
+    """
+    Resize and crop a 1024x768 RGB image to a 400x400 grayscale image
+
+    Args:
+    - np_image (np.ndarray): input image as a 3D NumPy array
+                             of shape (768, 1024, 3) with RGB channels
+
+    Returns:
+    - np.ndarray: cropped grayscale image as a 3D NumPy array
+                  of shape (400, 400, 1), or None if an error occurs
+
+    Raises:
+    - ValueError: if the input image is None, has an invalid shape,
+                  or does not match the required dimensions
+    """
     try:
         if np_image is None:
             raise ValueError("Image is None")
@@ -10,6 +24,31 @@ def zoom(np_image: np.ndarray) -> np.ndarray:
             raise ValueError("Image must be a 3D array")
         if np_image.shape[2] != 3:
             raise ValueError("Image must have 3 channels (RGB)")
+        if not (np_image.shape[0] == 768 or np_image.shape[1] == 1024):
+            raise ValueError("Image size must be 1024x768")
+
+        # Converts the image to grayscale using standard luminance weights
+        gray_np_image = np.dot(np_image[..., :3], [0.299, 0.587, 0.114])
+        # # Converts the image to grayscale by averaging the RGB channels
+        # gray_np_image = np.mean(np_image, axis=2)
+
+        # Define cropping starting points
+        start_x = (gray_np_image.shape[1] - 574)
+        start_y = (gray_np_image.shape[0] - 668)
+
+        # Crop the grayscale image to a 400x400
+        cropped_np_image = gray_np_image[
+            start_y:start_y + 400,
+            start_x:start_x + 400]
+
+        # Transpose, convert to uint8, and reshape for the output
+        cropped_np_image = np.transpose(cropped_np_image)
+        cropped_np_image = cropped_np_image.astype(np.uint8)
+        cropped_np_image = np.reshape(cropped_np_image, (400, 400, 1))
+
+        print(f'New shape after slicing: {cropped_np_image.shape}')
+
+        return cropped_np_image
     except Exception as e:
         print(f'zoom(): {e}')
         return None
