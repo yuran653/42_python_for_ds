81b0aa92171b9429ae94ce812f2bd9ea08da7bb9
Iurii Ovsiannikov <jgoldste@z1t6c2.42bangkok.com>
2025-01-24 13:58:48 +0700

    ex02 is done; *.jpg added to .gitignore; pillow lib added to requirements.txt

---
+++ python01/ex02/load_image.py
@@ -0,0 +1,29 @@
+import numpy as np
+import PIL.Image
+
+
+def ft_load(path: str) -> np.ndarray:
+    """
+    Loads an image from the specified path and returns it as a NumPy array
+
+    Parameters:
+    - path (str): the file path to the image
+
+    Returns:
+    - np.ndarray: a NumPy array representation of the image,
+                  or None if an error occurs
+
+    Raises:
+    - TypeError: if the provided path is not a string
+    - Exception: for any other errors encountered during image loading
+    """
+    try:
+        if type(path) is not str:
+            raise TypeError("'path' must be a string")
+        with PIL.Image.open(path) as image:
+            np_image = np.array(image)
+            print(f'The shape of image is: {np_image.shape}')
+            return np_image
+    except Exception as e:
+        print(f'ft_load(): {e}')
+        return None
